6a51042f50482eca9b483909997b20f5cfee1148
George Treviranus <geowtrev@gmail.com>
2025-01-29 10:49:05 -0600

    feat: configures fonts and theme

--- config/tailwind.ts
+++ config/tailwind.ts
@@ -8,17 +8,46 @@ import { tailwindTypography } from "./tailwind-typography"
 export const TAILWIND_THEME: Partial<CustomThemeConfig> = {
   typography: tailwindTypography,
 
-  // TODO: add navy and green palette
+  fontFamily: {
+    sans: ["Poppins", "sans-serif"],
+    heading: ["League Spartan", "sans-serif"],
+  },
+
+  colors: {
+    navy: {
+      50: "#E7F3FE",
+      100: "#D4E9FC",
+      200: "#A4D0F9",
+      300: "#78BAF7",
+      400: "#48A1F4",
+      500: "#1D8BF1",
+      600: "#0D71CF",
+      700: "#0A59A4",
+      800: "#074074", // primary shade
+      900: "#03203A",
+      950: "#02101D",
+    },
+    green: {
+      50: "#E5FFF4",
+      100: "#C7FFE8",
+      200: "#8FFFD0",
+      300: "#5CFFBB",
+      400: "#24FFA4",
+      500: "#00EB89",
+      600: "#00B367", // primary shade
+      700: "#00854D",
+      800: "#005C36",
+      900: "#002E1B",
+      950: "#00140C",
+    },
+  },
 }
 
 // DaisyUI custom themes
 
 export const DEFAULT_THEME = {
   ...light,
-  // primary: "#FF00F0",
-  // secondary: "#FF257C",
-  // accent: "#FF4518",
-  // "base-100": "#FFFCFC",
-  // "base-200": "#ECEFFF",
-  // "base-300": "#D3DAFF",
+  primary: "#074074",
+  secondary: "#00B367",
+  "base-content": "#011527",
 }
